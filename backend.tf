d0f541c2dc075c3928d4c13e0a6e5011e31975f5
Shivansh Mishra <autocodebuddy@gmail.com>
2025-11-25 18:00:28 +0530

    change .\backend.tf config

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "http" {
+  backend "https" {
     address        = "https://firefly-tfstate-backend/state/gcp-vm"
     lock_address   = "https://firefly-tfstate-backend/lock/gcp-vm"
     unlock_address = "https://firefly-tfstate-backend/unlock/gcp-vm"

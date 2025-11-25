c76df461130c0c1d0b6a7014808b87e6646d92ca
Shivansh Mishra <autocodebuddy@gmail.com>
2025-11-25 16:45:06 +0530

    updated config

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "http" {
+    address        = "https://firefly-tfstate-backend/state/gcp-vm"
+    lock_address   = "https://firefly-tfstate-backend/lock/gcp-vm"
+    unlock_address = "https://firefly-tfstate-backend/unlock/gcp-vm"
+    lock_method    = "POST"
+    unlock_method  = "POST"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+resource "google_compute_network" "network" {
+  name = "vm-base-network"
+}
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+  network_interface {
+    network = google_compute_network.network.self_link
+    access_config {}
+  }
+  tags = ["demo", "ci-cd"]
+}

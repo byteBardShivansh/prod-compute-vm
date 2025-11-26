512618bcfe660863ee3ea71dd3012af759ef1005
Shivansh Mishra <autocodebuddy@gmail.com>
2025-11-26 17:45:23 +0530

    Format Terraform files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "firefly-tfstate-backend"
-    prefix = "gcp-vm"          # folder-like path inside the bucket
+    prefix = "gcp-vm" # folder-like path inside the bucket
   }
 }
